519b49d4f86d2e5f626063fc07ea6d92c09c74a0
Ármin <csutorasa@gmail.com>
2023-09-05 23:56:41 +0200

    Add arduino core

--- cores/m12864stk/M12864stk.h
+++ cores/m12864stk/M12864stk.h
@@ -8,10 +8,4 @@
  */
 #define F_CPU 8000000L
 
-#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
-#define bitSet(value, bit) ((value) |= (1UL << (bit)))
-#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
-#define bitToggle(value, bit) ((value) ^= (1UL << (bit)))
-#define bitWrite(value, bit, bitvalue) ((bitvalue) ? bitSet(value, bit) : bitClear(value, bit))
-
 #endif // M12864STK
